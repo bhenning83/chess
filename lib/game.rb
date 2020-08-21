c0a02b1992776f3ef241f95c996736279091275a
Brendon Henning <brendonhenning@gmail.com>
2020-08-21 09:11:26 -0500

    add playable module to files

--- lib/game.rb
+++ lib/game.rb
@@ -15,30 +15,16 @@
 #   get user input on where to move it to
 #   check if it's valid
 #   change coordinate of the piece
-
+require_relative 'playable'
 class Game
+  include playable
   attr_accessor :board
+
   def initialize
     @board = make_board
   end
   
-  def make_board
-    hash = {}
-    (0..7).each do |i|
-      hash[i] = Array.new(8, ' - ')
-    end
-    hash
-  end
-
-  def display_board
-    board.each do |_key, value|
-      puts
-      value.each do |chip|
-        print chip
-      end
-    end
-    puts
-  end
+ 
 end
 
 game = Game.new
---
+++ lib/rook.rb
@@ -0,0 +1,35 @@
+require_relative 'playable'
+class Rook
+  include Playable
+  attr_accessor :pos
+  
+  def initialize(pos, color)
+    @pos = pos
+    @color = color
+  end
+
+  def valid?(new_spot)
+    return true if new_spot[0] == @pos[0]
+    return true if new_spot[1] == @pos[1]
+    false
+  end
+  
+  def open?(new_spot)
+    if new_spot[0] == @pos[0]
+      for i in new_spot[1]..@pos[1] #if rook is moving vertically
+        return false if board[0][i] != ' - '
+      end
+    elsif new_spot[1] == @pos[1] #if rook is moving horizontally
+      for i in new_spot[0]..@pos[0]
+        return false if board[i][0] != ' - '
+      end
+    end
+    true
+  end
+end
+
+  rook = Rook.new([1, 1], 'black')
+  puts rook.valid?([3, 4])
+  puts rook.valid?([5, 1])
+  puts rook.open?([1, 6])
+  puts rook.open?([5, 2])
