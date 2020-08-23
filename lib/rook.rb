903b8505d6aa9d591dade76397486b5bf4bd5029
Brendon Henning <brendonhenning@gmail.com>
2020-08-22 21:16:03 -0500

    reverse board agruments in false statement in clear?

--- lib/rook.rb
+++ lib/rook.rb
@@ -22,12 +22,11 @@ class Rook < Game
   def clear?(new_spot)
     if new_spot[0] == @pos[0]
       for i in new_spot[1]...@pos[1] #if rook is moving vertically
-        binding.pry
-          return false unless board[pos[0]][i] == ' - '
+          return false unless board[i][pos[0]] == ' - '
       end
     elsif new_spot[1] == @pos[1] #if rook is moving horizontally
       for i in @pos[0]...new_spot[0]
-        return false unless board[i][pos[0]] == ' - '
+        return false unless board[pos[0]][i] == ' - '
       end
     end
     true
@@ -35,8 +34,8 @@ class Rook < Game
 end
 
   game = Game.new
-  rook = Rook.new([1, 2], 'black', game.board)
+  rook = Rook.new([1, 7], 'black', game.board)
   game.board[7][1] = ' x '
   game.board[5][1] = King.new([1, 5], 'black', game.board)
-  puts rook.clear?([1, 7])
+  puts rook.clear?([1, 2])
   p game.display_board
