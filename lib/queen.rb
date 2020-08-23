abf9df370d47547e33d989e1b53d9cba42e63213
Brendon Henning <brendonhenning@gmail.com>
2020-08-22 23:10:31 -0500

    fixed clear_diag? and clear_vh?

--- lib/queen.rb
+++ lib/queen.rb
@@ -1,13 +1,16 @@
 require_relative 'playable'
 require_relative 'game'
+require 'pry'
+
 class Queen < Game
   include Playable
-  attr_accessor :pos, :board
+  attr_accessor :pos, :board, :values
   
   def initialize(pos, color, board)
     @pos = pos
     @color = color
     @board = board
+    @values = values
   end
 
   def valid?(new_spot)
@@ -34,35 +37,42 @@ class Queen < Game
         current[0] += 1; current[1] -= 1
         return true if current == new_spot
       elsif new_spot[0] < current[0] && new_spot[1] < current[1] #moving down and left
-        current[0] -= 1; current[1] -= 1
         return true if current == new_spot
+        current[0] -= 1; current[1] -= 1
+        binding.pry
       else new_spot[0] < current[0] && new_spot[1] > current[1] #moving up and left
         current[0] -= 1; current[1] += 1
         return true if current == new_spot
       end
     end
+    binding.pry
     false
   end
 
   def clear_vh?(new_spot)
-    if new_spot[0] == @pos[0]
-      for i in new_spot[1]..@pos[1] #if rook is moving vertically
-        return false unless board[0][i] == ' - '
+    if new_spot[0] == @pos[0] #if queen is moving vertically
+      endpoints(new_spot, 1)
+      for i in values[0]...values[1] 
+          return false unless board[i][pos[0]] == ' - '
       end
-    elsif new_spot[1] == @pos[1] #if rook is moving horizontally
-      for i in new_spot[0]..@pos[0]
-        return false unless board[i][0] == ' - '
+    elsif new_spot[1] == @pos[1] #if queen is moving horizontally
+      endpoints(new_spot, 0)
+      for i in values[0]...values[1]
+        return false unless board[pos[1]][i] == ' - '
       end
     end
     true
+  end
+
+  def endpoints(new_spot, idx)
+    values << new_spot[idx]
+    values << pos[idx]
+    values.sort!
   end
 
 
 end
 game = Game.new
 queen = Queen.new([5, 5], 'black', game.board)
-puts queen.valid?([3, 3])
-puts queen.valid?([5, 1])
-puts queen.clear?([1, 6])
-puts queen.valid?([1, 6])
+game.board[2][2] = Queen.new([2, 2], 'black', game.board)
 puts queen.clear?([1, 1])
