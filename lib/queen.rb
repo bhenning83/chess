b5a0f873a304efa94e8c5a4cd1841772e5194246
Brendon Henning <brendonhenning@gmail.com>
2020-08-23 13:51:45 -0500

    broke up the clear? method

--- lib/queen.rb
+++ lib/queen.rb
@@ -10,7 +10,6 @@ class Queen < Game
     @pos = pos
     @color = color
     @board = board
-    @values = values
   end
 
   def valid?(new_spot)
@@ -23,56 +22,76 @@ class Queen < Game
   end
 
   def clear?(new_spot)
-    return true if clear_diag?(new_spot) || clear_vh?(new_spot)
-    false
+    if new_spot[0] == pos[0]
+      clear_v?(new_spot) 
+    elsif new_spot[1] == pos[1]
+      clear_h?(new_spot) 
+    elsif new_spot[1] > pos[1]
+      clear_du?(new_spot)
+    elsif new_spot[1] < pos[1]
+      clear_dd?(new_spot)
+    end
   end
-  
-  def clear_diag?(new_spot)
+
+  def clear_du?(new_spot)
     current = pos.dup
     until board[current[0]].nil? || board[current[1]].nil? || board[current[1]][current[0]] != ' - '
-      if new_spot[0] > current[0] && new_spot[1] > current[1] #moving up and right
+      if new_spot[0] > current[0] #moving up and right
         current[0] += 1; current[1] += 1
         return true if current == new_spot
-      elsif new_spot[0] > current[0] && new_spot[1] < current[1] #moving down and right
-        current[0] += 1; current[1] -= 1
-        return true if current == new_spot
-      elsif new_spot[0] < current[0] && new_spot[1] < current[1] #moving down and left
-        return true if current == new_spot
-        current[0] -= 1; current[1] -= 1
-        binding.pry
-      else new_spot[0] < current[0] && new_spot[1] > current[1] #moving up and left
+      else #moving up and left
         current[0] -= 1; current[1] += 1
         return true if current == new_spot
       end
     end
-    binding.pry
     false
   end
 
-  def clear_vh?(new_spot)
-    if new_spot[0] == @pos[0] #if queen is moving vertically
-      endpoints(new_spot, 1)
-      for i in values[0]...values[1] 
-          return false unless board[i][pos[0]] == ' - '
-      end
-    elsif new_spot[1] == @pos[1] #if queen is moving horizontally
-      endpoints(new_spot, 0)
-      for i in values[0]...values[1]
-        return false unless board[pos[1]][i] == ' - '
+  def clear_dd?(new_spot)
+    current = pos.dup
+    until board[current[0]].nil? || board[current[1]].nil? || board[current[1]][current[0]] != ' - '
+      if new_spot[0] > current[0] #moving down and right
+        current[0] += 1; current[1] -= 1
+        return true if current == new_spot
+      else #moving down and left
+        current[0] -= 1; current[1] -= 1
+        return true if current == new_spot
       end
     end
-    true
+    false
   end
 
-  def endpoints(new_spot, idx)
-    values << new_spot[idx]
-    values << pos[idx]
-    values.sort!
+  def clear_v?(new_spot)
+    current = pos.dup
+    until board[current[0]].nil? || board[current[1]].nil? || board[current[1]][current[0]] != ' - '
+      if new_spot[1] > current[1] #moving up
+        current[1] += 1
+        return true if current == new_spot
+      else #moving down
+        current[1] -= 1
+        return true if current == new_spot
+      end
+    end
+    false
   end
 
+  def clear_h?(new_spot)
+    current = pos.dup
+    until board[current[0]].nil? || board[current[1]].nil? || board[current[1]][current[0]] != ' - '
+      if new_spot[0] > current[0] #moving right
+        current[0] += 1
+        return true if current == new_spot
+      else  #moving left
+        current[0] -= 1
+        return true if current == new_spot
+      end
+    end
+    false
+  end
 
 end
 game = Game.new
 queen = Queen.new([5, 5], 'black', game.board)
 game.board[2][2] = Queen.new([2, 2], 'black', game.board)
 puts queen.clear?([1, 1])
+puts queen.clear?([5, 0])
