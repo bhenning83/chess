f7b7eb501d9b448515b73ba15e68362e15eabd17
Brendon Henning <brendonhenning@gmail.com>
2020-08-22 22:42:36 -0500

    add endpoints method and fixed clear?

--- lib/rook.rb
+++ lib/rook.rb
@@ -5,12 +5,13 @@ require 'pry'
 
 class Rook < Game
   include Playable
-  attr_accessor :pos, :board
+  attr_accessor :pos, :board, :values
 
   def initialize(pos, color, board)
     @pos = pos
     @color = color
     @board = board
+    @values = []
   end
 
   def valid?(new_spot)
@@ -20,22 +21,29 @@ class Rook < Game
   end
   
   def clear?(new_spot)
-    if new_spot[0] == @pos[0]
-      for i in new_spot[1]...@pos[1] #if rook is moving vertically
+    if new_spot[0] == @pos[0] #if rook is moving vertically
+      endpoints(new_spot, 1)
+      for i in values[0]...values[1] 
           return false unless board[i][pos[0]] == ' - '
       end
     elsif new_spot[1] == @pos[1] #if rook is moving horizontally
-      for i in @pos[0]...new_spot[0]
-        return false unless board[pos[0]][i] == ' - '
+      endpoints(new_spot, 0)
+      for i in values[0]...values[1]
+        return false unless board[pos[1]][i] == ' - '
       end
     end
     true
   end
+
+  def endpoints(new_spot, idx)
+    values << new_spot[idx]
+    values << pos[idx]
+    values.sort!
+  end
 end
 
   game = Game.new
-  rook = Rook.new([1, 7], 'black', game.board)
-  game.board[7][1] = ' x '
-  game.board[5][1] = King.new([1, 5], 'black', game.board)
-  puts rook.clear?([1, 2])
+  rook = Rook.new([7, 5], 'black', game.board)
+  game.board[5][5] = King.new([5, 5], 'black', game.board)
+  puts rook.clear?([1, 5])
   p game.display_board
