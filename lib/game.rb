e98fa6a9386ba42ab8e8c1658fc25f38a9407aa9
Brendon Henning <brendonhenning@gmail.com>
2020-08-23 20:48:00 -0500

    change pseudocode

--- lib/game.rb
+++ lib/game.rb
@@ -1,12 +1,12 @@
 # create a board
 # write move method for each piece
 #   each piece will need to know all of it's possible moves
-#     After each move, every piece must loop through all its possible moves to see if king is in check
-#       if the king is in check, loop through each possible move of each remaining piece on king's team
-#         at each possible move, check every possible move from every piece on opponent's side
-#         if king is still in check, move to next possible move on king's side
-#         break if king is never in check after checking a single possible move
-#         declare checkmate if all possible moves on king's side are exhausted and king remains in check
+#     After each move, every piece must see if the opposing king is in it's valid, clear path
+#       if the king is in check, loop through each possible move of each remaining piece on defensive team
+#         at each possible defensive move, check every possible offensive move from every offensive piece
+#         if king is still in check, move to next possible defensive move on king's side
+#         break if king is never in check after checking every offensive move against a single defensive move
+#         declare checkmate if all possible defensive moves on king's side are exhausted and king remains in check
 # all game pieces can overtake opponent in their legal path, except pawns
 # players will have to first input the coord of the piece they want to move
 #   each player will need a piece_log
