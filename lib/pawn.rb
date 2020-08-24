1aa66de375f59fffce7dc88ce22c7eecd7946fc5
Brendon Henning <brendonhenning@gmail.com>
2020-08-24 18:39:00 -0500

    add find_poss_move

--- lib/pawn.rb
+++ lib/pawn.rb
@@ -5,12 +5,14 @@ require 'pry'
 
 class Pawn < Game
   include Playable
-  attr_accessor :pos, :board, :color
+  attr_accessor :pos, :board, :poss_moves
+  attr_reader :color
 
   def initialize(pos, color, board)
     @pos = pos
     @color = color
     @board = board
+    @poss_moves = []
   end
 
   def valid?(new_spot)
@@ -25,23 +27,71 @@ class Pawn < Game
 
   def check?
     if color == 'white'
-      binding.pry
       return if pos[1] == 7
-      return true if board[pos[1] - 1][pos[0] + 1].is_a?(King)
-      return true if board[pos[1] - 1][pos[0] - 1].is_a?(King)
-    else 
-      return if pos[1] == 0
       return true if board[pos[1] + 1][pos[0] + 1].is_a?(King)
       return true if board[pos[1] + 1][pos[0] - 1].is_a?(King)
+    else 
+      return if pos[1] == 0
+      return true if board[pos[1] - 1][pos[0] + 1].is_a?(King)
+      return true if board[pos[1] - 1][pos[0] - 1].is_a?(King)
     end
     false
+  end
+
+  def find_poss_move
+    find_poss_move_black if color == 'black'
+    find_poss_move_white if color == 'white'
+  end
+
+  def find_poss_move_white
+    return if pos[1] == 7
+    poss_moves << [pos[0], pos[1] + 1]
+    if pos[1] == 1
+      poss_moves << [pos[0], pos[1] + 2]
+    end
+    possible_attacks_white.each do |spot|
+      poss_moves << spot
+    end
+  end
+
+  def possible_attacks_white(pieces = [], attackable = [])
+    pieces << board[pos[1] + 1][pos[0] + 1] unless pos[0] == 7
+    pieces << board[pos[1] + 1][pos[0] - 1] unless pos[0] == 0
+    pieces.each do |piece|
+      next if piece == ' - '
+      attackable << piece.pos if piece.color == 'black'
+    end
+    attackable
+  end
+
+  def find_poss_move_black
+    return if pos[1] == 0
+    poss_moves << [pos[0], pos[1] - 1]
+    if pos[1] == 6
+      poss_moves << [pos[0], pos[1] - 2]
+    end
+    possible_attacks_black.each do |spot|
+      poss_moves << spot
+    end
+  end
+
+  def possible_attacks_black(pieces = [], attackable = [])
+    pieces << board[pos[1] - 1][pos[0] + 1] unless pos[0] == 7
+    pieces << board[pos[1] - 1][pos[0] - 1] unless pos[0] == 0
+    pieces.each do |piece|
+      next if piece == ' - '
+      attackable << piece.pos if piece.color == 'white'
+    end
+    attackable
   end
 end
 
 game = Game.new
-pawn = Pawn.new([2,7], 'white', game.board)
-p pawn.valid?([2, 8])
-p pawn.valid?([5, 0])
-game.board[7][2] = King.new('black', [3, 8], game.board)
+pawn = Pawn.new([0, 1], 'white', game.board)
+game.board[5][6] = King.new([6, 5], 'white', game.board)
 p pawn.check?
-p game.board
+pawn.find_poss_move
+p pawn.poss_moves
+bpawn = Pawn.new([7, 6], 'black', game.board)
+bpawn.find_poss_move
+p bpawn.poss_moves
