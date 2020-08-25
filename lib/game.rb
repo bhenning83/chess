5d5f8e6350c21592a5e9b39f2064263af1a680d0
Brendon Henning <brendonhenning@gmail.com>
2020-08-25 12:28:10 -0500

    add set_board adn find_black_king

--- lib/game.rb
+++ lib/game.rb
@@ -17,12 +17,91 @@
 #   change coordinate of the piece
 
 require_relative 'playable'
+require_relative 'clearable'
+require_relative 'knight'
+require_relative 'rook'
+require_relative 'bishop'
+require_relative 'queen'
+require_relative 'king'
+require_relative 'pawn'
+
+require 'pry'
 
 class Game
   include Playable
-  attr_accessor :board
-
+  attr_accessor :board, :white_pieces, :black_pieces
+  
   def initialize
     @board = make_board
+    @white_pieces = []
+    @black_pieces = []
+  end
+
+  def make_white_army
+    pawn1w = Pawn.new([0, 1], 'white', board, ' ♟︎ ')
+    pawn2w = Pawn.new([1, 1], 'white', board, ' ♟︎ ')
+    pawn3w = Pawn.new([2, 1], 'white', board, ' ♟︎ ')
+    pawn4w = Pawn.new([3, 1], 'white', board, ' ♟︎ ')
+    pawn5w = Pawn.new([4, 1], 'white', board, ' ♟︎ ')
+    pawn6w = Pawn.new([5, 1], 'white', board, ' ♟︎ ')
+    pawn7w = Pawn.new([6, 1], 'white', board, ' ♟︎ ')
+    pawn8w = Pawn.new([7, 1], 'white', board, ' ♟︎ ')
+    white_pieces << pawn1w << pawn2w << pawn3w << pawn4w << pawn5w << pawn6w << pawn7w << pawn8w
+    rook1w = Rook.new([0, 0], 'white', board, ' ♜ ')
+    rook2w = Rook.new([7, 0], 'white', board, ' ♜ ')
+    knight1w = Knight.new([1, 0], 'white', board, ' ♞ ')
+    knight2w = Knight.new([6, 0], 'white', board, ' ♞ ')
+    bishop1w = Bishop.new([2, 0], 'white', board, ' ♝ ')
+    bishop2w = Bishop.new([5, 0], 'white', board, ' ♝ ')
+    queenw = Queen.new([3, 0], 'white', board, ' ♛ ')
+    kingw = King.new([4, 0], 'white', board, ' ♚ ')
+    white_pieces << rook1w << rook2w << knight1w << knight2w << bishop1w << bishop2w << queenw << kingw
+  end
+
+  def make_black_army
+    pawn1b = Pawn.new([0, 6], 'black', board, ' ♙ ')
+    pawn2b = Pawn.new([1, 6], 'black', board, ' ♙ ')
+    pawn3b = Pawn.new([2, 6], 'black', board, ' ♙ ')
+    pawn4b = Pawn.new([3, 6], 'black', board, ' ♙ ')
+    pawn5b = Pawn.new([4, 6], 'black', board, ' ♙ ')
+    pawn6b = Pawn.new([5, 6], 'black', board, ' ♙ ')
+    pawn7b = Pawn.new([6, 6], 'black', board, ' ♙ ')
+    pawn8b = Pawn.new([7, 6], 'black', board, ' ♙ ')
+    black_pieces << pawn1b << pawn2b << pawn3b << pawn4b << pawn5b << pawn6b << pawn7b << pawn8b
+    rook1b = Rook.new([0, 7], 'white', board, ' ♖ ')
+    rook2b = Rook.new([7, 7], 'white', board, ' ♖ ')
+    knight1b = Knight.new([1, 7], 'white', board, ' ♘ ')
+    knight2b = Knight.new([6, 7], 'white', board, ' ♘ ')
+    bishop1b = Bishop.new([2, 7], 'white', board, ' ♗ ')
+    bishop2b = Bishop.new([5, 7], 'white', board, ' ♗ ')
+    queenb = Queen.new([3, 7], 'white', board, ' ♕ ')
+    kingb = King.new([4, 7], 'white', board, ' ♔ ')
+    black_pieces << rook1b << rook2b << knight1b << knight2b << bishop1b << bishop2b << queenb << kingb
+  end
+
+  def set_board
+    white_pieces.each do |piece|
+      board[piece.pos[1]][piece.pos[0]] = piece.symbol
+    end
+    black_pieces.each do |piece|
+      board[piece.pos[1]][piece.pos[0]] = piece.symbol
+    end
+  end
+
+  def find_black_king
+    board.each do |key, value|
+      if value.include?(' ♔ ')
+         idx = value.index(' ♔ ')
+         binding.pry
+         return [idx, key]
+      end
+    end
   end
 end
+
+game = Game.new
+game.make_white_army
+game.make_black_army
+game.set_board
+game.display_board
+p game.find_black_king
