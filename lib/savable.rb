d978c77501ea162cfa37d28b68dcdc12eaf71746
Brendon Henning <brendonhenning@gmail.com>
2020-09-03 09:30:21 -0500

    second possible save solution

--- lib/savable.rb
+++ lib/savable.rb
@@ -4,27 +4,24 @@ require 'pry'
 module Savable
   def to_json(*_args)
     JSON.dump({
-                white_pieces: breakdown(@white_pieces),
-                black_pieces: breakdown(@black_pieces)
+                pos: @pos,
+                color: @color,
+                poss_moves: @poss_moves,
+                symbol: @symbol
               })
   end
 
-  def breakdown(piece_list, hash = {})
-    return if piece_list.nil?
-    piece_list.each do |piece|
-      hash[piece] = Hash.new
-      piece.instance_variables.each do |var|
-        next if var == :@board
-        hash[piece][var] = piece.instance_variable_get var
-      end
-    end
-    hash
+  def save_piece_lists
+    JSON.dump({
+                white_pieces: @white_pieces.each { |piece| piece.to_json},
+                black_pieces: @black_pieces.each { |piece| piece.to_json}
+              })
   end
 
   def save
     puts 'Enter a name for your saved game.'
     save_name = gets.chomp
-    stream = to_json
+    stream = save_piece_lists
     File.open(@path + save_name, 'w') { |f| f.puts stream }
     puts 'Game saved. Do you want to quit your game? yes/no'
     if gets.chomp.downcase == 'yes'
@@ -37,20 +34,22 @@ module Savable
     puts 'What is the name of your saved game?'
     answer = gets.chomp
     save = File.read(@path + answer)
+    binding.pry
     data = JSON.parse(save)
     binding.pry
-    @white_pieces = reassign(data['white_pieces'])
-    @black_pieces = reassign(data['black_pieces'])
-   
+    load_piece_lists(save)
     @loaded = true
   end
 
-  def reassign(hash)
-    hash.each do |_name, values|
-      values.each do |var, val|
-        instance_variable_set var, val
-      end
-    end
+  def create_from_json(data)
+    self.new(data['pos'], data['color'], data['poss_moves'], data['symbol'])
+  end
+
+  def load_piece_lists(data)
+    test = JSON.parse(data)
+    binding.pry
+    @white_pieces = test['white_pieces'].each {|piece| piece.create_from_json(test)}
+    @black_pieces = test['black_pieces'].each {|piece| piece.create_from_json(test)}
   end
 
   def ask_to_load
